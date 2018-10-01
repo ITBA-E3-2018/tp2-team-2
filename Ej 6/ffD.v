bcb29a727ed2daba299584dd511845fbf5a0e2a0
Victor Oh <voh@itba.edu.ar>
2018-10-01 19:44:13 -0300

    Empece con el ej 6.

---
+++ Ej 6/ffD.v
@@ -0,0 +1,28 @@
+module flipD(
+    D,  //Entry
+    E,  //Enable
+    Q,  //Output
+    nQ  //Neg-Output
+);
+
+//Defino Inputs
+input D;
+input E;
+
+//Defino OUtputs
+output Q;
+output nQ;
+
+//Defino Parametros
+wire D,E,Q,nQ;
+
+wire a,b,c;
+
+//Code
+not(a,D);
+and(b,a,E);
+and(c,E,D);
+nor(Q,b,nQ);
+nor(nQ,c,Q);
+
+endmodule
